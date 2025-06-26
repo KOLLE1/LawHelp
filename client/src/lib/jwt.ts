67411e7201c75f7025d266ae08f1358d440612b0
KOLLE KANG BRANDON EPIE <129511361+KOLLE1@users.noreply.github.com>
2025-06-26 14:19:50 +0100

    Refactor JWT_SECRET initialization to provide a default value if not set in environment variables

--- client/src/lib/jwt.ts
+++ client/src/lib/jwt.ts
@@ -1,5 +1,1 @@
-export const JWT_SECRET = process.env.JWT_SECRET!;
-
-if (!JWT_SECRET) {
-  throw new Error("❌ JWT_SECRET is not set in .env");
-}
+export const JWT_SECRET = process.env.JWT_SECRET! || "ljankfbauifiausfjbdjsjdfjksdbfka"
